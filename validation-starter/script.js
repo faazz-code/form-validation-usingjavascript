262d32cf93ea4ded07b258277ae382ef91a104c6
faazz-code <fzkodur@gmail.com>
2025-01-17 00:00:51 +0530

    Your commit message

---
+++ validation-starter/script.js
@@ -0,0 +1,19 @@
+var nameError = document.getElementById('name-error')
+var phoneError = document.getElementById('name-error')
+var emailError = document.getElementById('name-error')
+var messageError = document.getElementById('name-error')
+var submitError = document.getElementById('name-error')
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if (name.length== 0){
+        nameError.innerHTML= "Name is Required";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)){
+        nameError.innerHTML= "write full name";
+    }
+    nameError.innerHTML= '<i class="fa-solid fa-check"></i>';
+    return true;
+}
